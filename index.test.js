b482f82048c90320239edde82792aa8a201b0f51
Kiriii <Kiriii@protonmail.com>
2022-05-28 08:06:28 +0200

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,3 +1,34 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@JKiriii 
+JKiriii
+/
+.github-javascript-action
+Public template
+generated from actions/javascript-action
+Code
+Issues
+Pull requests
+Discussions
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+.github-javascript-action/index.test.js /
+@JKiriii
+JKiriii Initial commit
+Latest commit 18f89dc 5 days ago
+ History
+ 1 contributor
+24 lines (21 sloc)  750 Bytes
+   
 const wait = require('./wait');
 const process = require('process');
 const cp = require('child_process');
@@ -22,3 +53,15 @@ test('test runs', () => {
   const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
   console.log(result);
 })
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
